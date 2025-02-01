84f785b6058e0c6ef0db3a6bf68e9fa3b2530403
Flods2020 <florian.desieter2020@campus-eni.fr>
2025-02-01 01:29:00 +0100

    contactForm et mail sending 0128 010225

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,15 +4,17 @@ namespace App\Controller;
 
 use App\DTO\ContactDTO;
 use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
 
         $data = new ContactDTO();
@@ -24,7 +26,19 @@ class ContactController extends AbstractController
         $form = $this->createForm(ContactType::class, $data);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new TemplatedEmail())
+                ->from($data->email)
+                ->to('floriands09@gmail.com')
+                ->subject('Test mail Cuisine App')
 
+                ->htmlTemplate('emails/contact.html.twig')
+                
+                ->context(['data' => $data]);
+
+                $mailer->send($email);
+                $this->addFlash('success', 'Message envoyé !');
+
+                return $this->redirectToRoute('contact');                
         }
         return $this->render('contact/contact.html.twig', [
             'form' => $form,
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -7,11 +7,9 @@ use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RecipeController extends AbstractController
 {
